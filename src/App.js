5afee435aed925cd15c7b2bd19d16308fff2e425
Jorge Barcos <jorgebarcos88@gmail.com>
2019-11-28 19:43:33 -0300

    Face Detection Box

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ import './App.css';
 
 const app = new Clarifai.App({
 	apiKey: '0c035c06f9c14e0caec302e8fc2cf57a'
-   });
-   
+});
 
 const particlesOptions = {
 	particles: {
@@ -29,26 +28,39 @@ class App extends Component {
 		super();
 		this.state = {
 			input: '',
-			imageUrl: ''
+			imageUrl: '',
+			box: {}
 		};
 	}
 
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputimage');
+		const width = Number(image.width);
+		const height = Number(image.height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - clarifaiFace.right_col * width,
+			bottomRow: height - clarifaiFace.bottom_row * height
+		};
+	};
+
+	displayFaceBox = (box) => {
+		this.setState({ box: box });
+	};
+
 	onInputChange = (event) => {
-		this.setState({input: event.target.value});
+		this.setState({ input: event.target.value });
 	};
 
 	onButtonSubmit = () => {
-		this.setState({imageUrl: this.state.input})
-		
-		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-		.then(
-    		function(response) {
-				console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-    		},
-			function(err) {
-			// there was an error
-			}
-		);
+		this.setState({ imageUrl: this.state.input });
+
+		app.models
+			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+			.then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+			.catch((err) => console.log(err));
 	};
 
 	render() {
@@ -59,7 +71,7 @@ class App extends Component {
 				<Logo />
 				<Rank />
 				<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-				<FaceRecognition imageUrl={this.state.imageUrl} /> 
+				<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
 			</div>
 		);
 	}
